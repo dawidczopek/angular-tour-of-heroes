9ac75260b8966cf68f8b273df3eb4ad196d961a2
Dawid <d.czopek@emotivo.pl>
2019-01-17 19:47:08 +0100

    Services in Angular

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Hero } from "../hero";
+import { Hero } from '../hero';
 
 @Component({
-  selector: 'app-hero-detail',
-  templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.scss']
+    selector: 'app-hero-detail',
+    templateUrl: './hero-detail.component.html',
+    styleUrls: ['./hero-detail.component.scss']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
 
-  constructor() { }
+    @Input() hero: Hero;
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+    }
 
 }
